faaf912ab6bb25c57929314693127531d3b2479c
AnimeFan0205 <Holden.Austin@pcastudent.me>
2021-07-26 08:43:09 -0700

    Bruh

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BenIsAwesome.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+public class BenIsAwesome {
+
+   public static void main(String args[]) {
+
+
+       int a = 1000;
+       int b = 1;
+
+       if (false) {
+           System.out.println("Hello world");
+       } else if (a < b) {
+           System.out.println("a is bigger than b!");
+       } else {
+           System.out.println("No conditions were true");
+       }
+
+   }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/firstRobotProgram.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp
+public class firstRobotProgram extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotorEx motor = hardwareMap.get(DcMotorEx.class, "FrontLeft");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            motor.setPower(0.1);
+            System.out.println(motor.getCurrentPosition());
+        }
+
+    }
+}
